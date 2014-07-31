caf9f6b0ed6693a23c58efe5444c677692b8f17b
Daniel Ranke <atrusberlin@gmail.com>
2014-07-31 21:41:52 +0200

    Initial commit with version 1.0

---
+++ README.md
@@ -0,0 +1,66 @@
+# Trello JavaScript Client
+
+This project has the goal to show a Trello Board as a simple view.
+
+The view is read only and allows any person to read the board even without an own Trello login.
+
+## How-To
+
+Import the JavaScript and CSS files also the jquery library.
+Define the following HTML snippet:
+
+```html
+<script language="JavaScript" src="lib/jquery-2.1.1.min.js"></script>
+<script language="JavaScript" src="trelloClient.js"></script>
+
+<script language="JavaScript">
+  var client = new TrelloClient([trello application key],[trello user token]);
+  client.descriptionEnabled = true; // default
+  client.dueDateEnabled = true; // default
+  client.showBoardTitle = true; // default
+  client.requestLists([boardId]);
+</script>
+
+<style type="text/css">
+  @import "trelloClient.css";
+</style>
+
+<span id="trelloError"></span>
+
+<div id="board" style="width:600px; height:400px">
+  <span id="info"> [enter the text you want to show] </span>
+  <div id="boardTitle"></div>
+  <div id="boardLists"></div>
+</div>
+```
+
+## Demo - trelloClient.html
+
+To run this demo you need to specify a `config.json` next to `trelloClient.html`
+```json
+{
+  "applicationKey" : "[yourApplicationKey]",
+  "userToken" : "[yourTokenAuthorizedByAUserHowHasAccessToTheBoard]",
+  "boardId" : "[theBoardId: e.g. cI66RoQS]"
+}
+```
+
+_**yourApplicationKey**_: [Generate your own application key](https://trello.com/1/appKey/generate)
+
+_**yourTokenAuthorizedByAUserHowHasAccessToTheBoard**_: `https://trello.com/1/authorize?key={YourAppKey}&name={YourAppName}&expiration=1day&response_type=token&scope=read`
+
+Public boards don't need neither **applicationKey** nor **userToken**.
+
+## License
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
---
+++ src/trelloClient.js
@@ -0,0 +1,119 @@
+function TrelloClient(appKey, userToken) {
+  var $this = this;
+  var keyAndToken = "";
+  if (appKey) {
+    keyAndToken = "key=" + appKey;
+  }
+  if (userToken) {
+    keyAndToken += "&token=" + userToken;
+  }
+
+  $this.dueDateEnabled = true;
+  $this.descriptionEnabled = true;
+  $this.showBoardTitle = true;
+  $this.avatarPrefix = "";
+
+  function handleFailure(data) {
+    $("#trelloError").html("Error (" + data.status + "): " + data.responseText)
+        .css("display", "block");
+  }
+
+  function shortDate(dateObject) {
+    var d = new Date(dateObject);
+    var day = d.getDate();
+    var month = d.getMonth() + 1;
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+    var date = day + "." + month;
+
+    return date;
+  }
+
+  function handleMemberResponse(data, cardId) {
+    var $members = $("<div class='cardMembers' id='cardMembers" + cardId + "'></div>");
+    for (i = 0; i < data.length; i++) {
+      var current = data[i]
+      var title = $this.avatarPrefix + current.fullName;
+      $members.append("<div class='avatar' title='" + title + "'>" + current.initials + "</div>");
+    }
+    $members.appendTo("#card" + cardId + " .cardDetails");
+  }
+
+  function requestMember(cardId) {
+    var membersRequest = "https://api.trello.com/1/card/" + cardId + "/members" + getKeyAndToken("?");
+    $.get(membersRequest, null, function (data) {
+      handleMemberResponse(data, cardId)
+    }, "json").fail(handleFailure);
+  }
+
+  function handleCardResponse(data) {
+    for (i = 0; i < data.length; i++) {
+      var current = data[i];
+      var listId = current.idList;
+      var $card = $("<div id='card" + current.id + "' class='card'><div>" + current.name + "</div></div>");
+      var $details = $("<div></div>").addClass("cardDetails");
+      var $data = $("<div></div>").addClass("cardData");
+      if ($this.descriptionEnabled && current.desc) {
+        var $desc = $("<div></div>").addClass("cardDescription").attr("title", current.desc);
+        $desc.appendTo($data);
+      }
+      if ($this.dueDateEnabled && current.due) {
+        var $dueDate = $("<div></div>");
+        $dueDate.addClass("dueDate").text(shortDate(current.due));
+        $dueDate.appendTo($data);
+      }
+      $data.appendTo($details); // Work in Progess, noch nicht darstellen
+      requestMember(current.id);
+      $details.appendTo($card);
+      $card.appendTo("#list" + listId);
+    }
+  }
+
+  function requestCards(listId) {
+    var cardRest = "https://api.trello.com/1/lists/" + listId + "/cards" + getKeyAndToken("?")
+    $.get(cardRest, null, handleCardResponse, "json").fail(handleFailure);
+  }
+
+  function handleListResponse(data) {
+    var $lists = $("#boardLists");
+    for (i = 0; i < data.length; i++) {
+      var current = data[i];
+      var divId = "list" + current.id;
+      var $list = $("<div id='" + divId + "' class='list'/>");
+      $list.append("<div class='listName'>" + current.name + "</div>");
+      requestCards(current.id);
+      $list.appendTo($lists);
+    }
+  }
+
+  function getKeyAndToken(prefix) {
+    if (keyAndToken) {
+      return prefix + keyAndToken;
+    }
+    return "";
+  }
+
+  this.loadBoard = function (boardId) {
+    var getBoard = "https://trello.com/1/boards/" + boardId + getKeyAndToken("/?");
+    var getLists = "https://trello.com/1/boards/" + boardId + "/lists" + getKeyAndToken("?");
+    var $info = $("#info");
+
+    if ($this.showBoardTitle) {
+      $.get(getBoard, null, function (data) {
+        $("#boardTitle").html(data.name);
+      }, "json");
+    }
+    else {
+      $("#boardTitle").remove();
+    }
+
+    $.get(getLists, null, function (data) {
+      handleListResponse(data);
+      $("#info").remove();
+    }, "json").fail(handleFailure);
+  }
+}

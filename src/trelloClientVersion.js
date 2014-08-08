f8779312a2ff0d1d654108fcdc6d76ed4177eede
Daniel Ranke <atrusberlin@gmail.com>
2014-08-08 15:37:46 +0200

    version info is enablebar, content not included; content should be defined in a seperate releasenotes.json

--- src/trelloClientVersion.js
+++ src/trelloClientVersion.js
@@ -1,9 +1,28 @@
 function TrelloClientVersion() {
 
   var $this = this;
+  var $versionInfo, $versionInfoText;
 
-  $this.showInfo = function () {
-    var $versionInfo = $.("#board");
-
+  $this.activate = function () {
+    $versionInfo = $("<div id='versionInfo'></div>").insertBefore("#board");
+    $versionInfoText = $("<div id='versionInfoText'></div>").insertAfter($versionInfo).hide();
+    initPopup();
   }
+
+  var initPopup = function () {
+    $versionInfoText.position({
+                                my: "left+10 top+10",
+                                at: "right bottom",
+                                of: "#versionInfo"
+                              })
+    $versionInfoText.hide();
+
+    $("#versionInfo").mouseover(function () {
+
+      $versionInfoText.show();
+    }).mouseout(function () {
+      $versionInfoText.hide();
+    });
+
+  };
 }

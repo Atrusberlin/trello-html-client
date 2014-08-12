2ae26fb39c5bd6a8ac00d16d5b5373eb1c13cb5e
Daniel Ranke <atrusberlin@gmail.com>
2014-08-12 13:13:13 +0200

    releasenotes.json is rendered into the popup

--- src/trelloClientVersion.js
+++ src/trelloClientVersion.js
@@ -1,12 +1,15 @@
-function TrelloClientVersion() {
+function TrelloClientVersion(versionInfoFile) {
 
   var $this = this;
   var $versionInfo, $versionInfoText;
 
   $this.activate = function () {
-    $versionInfo = $("<div id='versionInfo'></div>").insertBefore("#board");
-    $versionInfoText = $("<div id='versionInfoText'></div>").insertAfter($versionInfo).hide();
-    initPopup();
+    $.getJSON(versionInfoFile, function (data) {
+      $versionInfo = $("<div id='versionInfo'></div>").insertBefore("#board");
+      $versionInfoText = $("<div id='versionInfoText'></div>").insertAfter($versionInfo).hide();
+      initPopup();
+      fillPopup(data);
+    });
   }
 
   var initPopup = function () {
@@ -16,13 +19,52 @@ function TrelloClientVersion() {
                                 of: "#versionInfo"
                               })
     $versionInfoText.hide();
+    var $description = $("<div class='description'>");
+    $description.appendTo($versionInfoText);
+    $("<p>Project to get read-only access to an Trello board.</p>").appendTo($description);
+    $("<p>Sources @github: <a href='https://github.com/Atrusberlin/trello-html-client' target='_blank'>Atrusberlin/trello-html-client</a></p>").appendTo($description);
+
+    var $infoTextHasFocus = false;
 
     $("#versionInfo").mouseover(function () {
-
+      $infoTextHasFocus = false;
       $versionInfoText.show();
     }).mouseout(function () {
-      $versionInfoText.hide();
+      window.setTimeout(hideInfoText, 3000);
     });
 
+    /*    $versionInfoText.mouseover(function() {
+     $infoTextHasFocus = true;
+     }).mouseout(function() {
+     $infoTextHasFocus = false;
+     hideInfoText();
+     })*/
+
+    var hideInfoText = function () {
+      if (!$infoTextHasFocus) {
+        $versionInfoText.hide();
+      }
+    }
+
   };
+
+  var fillPopup = function (releaseData) {
+    var releases = releaseData.releases;
+    if (releases && releases.length > 0) {
+      for (i = 0; i < releases.length; i++) {
+        var release = releases[i];
+        $("<p>Version: " + release.number + "</p>").appendTo($versionInfoText);
+        if (release.changes) {
+          var changeList = $("<ul></ul>");
+          for (change = 0; change < release.changes.length; change++) {
+            var entry = release.changes[change];
+            $("<li>" + entry.desc + "</li>").appendTo(changeList);
+          }
+          changeList.appendTo($versionInfoText);
+        }
+
+      }
+    }
+
+  }
 }
